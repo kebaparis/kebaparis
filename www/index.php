b37dd6337199e41782abf13b80d4083c1a63b437
marco <marco@5th.ch>
2011-02-07 15:58:21 +0100

    outsourced login form

---
+++ www/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+  
+  
+    <link rel="stylesheet" type="text/css" href="main.css">
+    
+    <style type="text/css">
+    
+      /* ... Hier werden die Formate definiert ... */
+    
+    </style>
+  
+  
+  </head>
+
+  <body>
+    <div id="login">
+    
+      <?php include 'login_form.php'; ?>
+      
+    </div> <!-- end div #login -->
+  
+  
+  
+    <div id="drm">
+<table>
+  <tr>
+    <th>Kebap Basel</th>
+  </tr>
+  
+  <tr>
+    <td>blahh</td>
+  </tr>
+  
+  <tr>
+    <td>blahh</td>
+  </tr>
+  
+</table>
+    
+    </div>
+
+  
+  </body>
+
+</html>
