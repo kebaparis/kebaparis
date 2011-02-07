7695ab07211ce1250342f2f4d98ab58f2f28425c
marco <marco@5th.ch>
2011-02-07 14:34:53 +0100

    login form outsourced

---
+++ www/login_form.php
@@ -0,0 +1,21 @@
+      <table>
+        <tr>
+        
+          <td>
+            <form action="#" method="get">
+            <label>username<label>
+            <input type="text" name="username" />
+          </td>
+          
+          <td>
+            <label>password<label>
+            <input type="password" name="password" />
+          </td>
+          
+          <td>
+            <input type="submit" />
+            </form>
+          </td>
+          
+        </tr>
+      </table>
