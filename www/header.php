4276bed1de50479e9f3d2718cae8ce095d1c9230
Marcel Keller <marcel.keller1993@gmail.com>
2011-05-17 21:41:40 +0200

    added links to java scripts

--- www/header.php
+++ www/header.php
@@ -6,12 +6,25 @@
   
   
     <link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" href="css/coda-slider-2.0.css" type="text/css" media="screen" />
     
     <style type="text/css">
     
       /* ... #blahh {  } ... */
     
     </style>
-  
+	
+	<script type="text/javascript" src="js/ajax.js" ></script>
+	
+	<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="js/jquery.coda-slider-2.0.js"></script>
+	
+	<script type="text/javascript">
+		$().ready(function() {
+			$('#settings-slider').codaSlider();
+		});
+	</script>
+	
   
   </head>
