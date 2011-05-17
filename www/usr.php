8f99a3cd81626245a68dc0c24ec1aa3531839373
Marcel Keller <marcel.keller1993@gmail.com>
2011-05-17 21:43:18 +0200

    added login function with ajax

--- www/usr.php
+++ www/usr.php
@@ -1,115 +1,140 @@
 <?php 
-  
-  include 'classes.php';
-  $myDB = new Database();
-  $myDB->connect();
-  
-  //define request type (login / logout / registration / activation / blahh)
-  $rtype = $_REQUEST['rtype'];
-  
-  switch ($rtype) {
-    case "lin":
-      lin();
-      break;
-      
-    case "lou":
-      lou();
-      break;
-      
-    case "reg":
-      reg();
-      break;
-      
-    case "act":
-      act();
-      break;
-      
-    default:
-      def();
-      break;
-  }
-  
-  $myDB->quit();
-  
 
-  function lin() {
-    echo "login requested <br />";
-    $User['username'] = $_REQUEST['username'];
-    $User['password'] = $_REQUEST['password'];
-    
-    $myuser = new user($User['username']);
-    $myuser->login($User['password']);
-  
-  } // end lin()
+	include 'classes.php';
+	$myDB = new Database();
+	$myDB->connect();
 
-  function lou() {
-    echo "logout requested <br />";
-    //user class weiss den username aus der session
-    
-    $myuser = new user();
-    $myuser->logout();
-  
-  } // end lou()
+	//define request type (login / logout / registration / activation / blahh)
+	$rtype = $_REQUEST['rtype'];
+
+	switch ($rtype) 
+	{
+		case "lin": lin();
+					break;
+
+		case "lou":	lou();
+					break;
+
+		case "reg":	reg();
+					break;
+
+		case "act":	act();
+					break;
+
+		default:	def();
+					break;
+	}
+
+	$myDB->quit();
 
 
-  
-  function reg() {
-    echo "registration requested <br />";
-    $newUser['username'] = $_REQUEST['username'];
-    $newUser['password'] = $_REQUEST['password'];
-    $newUser['email'] = $_REQUEST['email'];
-    
-    $myuser = new user($newUser['username']);
-    $myuser->register($newUser['password'], $newUser['email']);
-  
-  } // end reg()
-  
-  
-  function act() {
-    echo "activation requested <br />";
-    $sentAkey = $_REQUEST['akey'];
-    
-    $myuser = new user();
-    $myuser->checkActivationLink($sentAkey);
-  
-  } // end act()
+	function lin() 
+	{
+		echo "login requested <br />";
+		$User['username'] = $_REQUEST['username'];
+		$User['password'] = $_REQUEST['password'];
+
+		$myuser = new user($User['username']);
+
+		if($myuser->login($User['password']) == true)
+		{
+			printLoggedInForm($myuser->usrName);
+		}
+		else
+		{
+			//show login form if login failed
+			echo "not logged in";
+			printLoginForm();
+		}
+
+	} // end lin()
+
+	function lou() 
+	{
+		echo "logout requested <br />";
+		//user class weiss den username aus der session
+
+		$myuser = new user();
+		$myuser->logout();
+
+		echo "logged out.";
+		printLoginForm();
+
+	} // end lou()
 
 
-  function def() {
-	echo "default requested <br />";
-    $myuser = new user();
-    
-    if ($myuser->checkLogin()) {
-      echo "logged in... $myuser->username";
-    }
-    else {
-      echo "user not logged in";
-    ?>
-<table>
-  <tr>
-  
-    <td>
-      <label>username<label>
-      <form action="#" method="get">
-      <input type="text" name="username" />
-    </td>
-    
-    <td>
-      <label>password<label>
-      <input type="password" name="password" />
-    </td>
-    
-    <td>
-      <input type="submit" value="login" />
-      </form>
-    </td>
-    
-  </tr>
-</table>
-    <?php
-    } //end else
-    
-  } // end def()
+
+	function reg() 
+	{
+		echo "registration requested <br />";
+		$newUser['username'] = $_REQUEST['username'];
+		$newUser['password'] = $_REQUEST['password'];
+		$newUser['email'] = $_REQUEST['email'];
+
+		$myuser = new user($newUser['username']);
+		$myuser->register($newUser['password'], $newUser['email']);
+
+	} // end reg()
+
+
+	function act() 
+	{
+		echo "activation requested <br />";
+		$sentAkey = $_REQUEST['akey'];
+
+		$myuser = new user();
+		$myuser->checkActivationLink($sentAkey);
+
+	} // end act()
+
+
+	function def() 
+	{
+		echo "default requested <br />";
+		$myuser = new user();
+
+		if ($myuser->checkLogin()) 
+		{
+			printLoggedInForm($myuser->usrName);
+		}
+		else 
+		{
+			echo "not logged in";
+			printLoginForm();
+		} //end else
+
+	} // end def()
+
+	// prints the login form (no check if user is logged in!!)
+	function printLoginForm()
+	{
+		?>
+		<table>
+			<tr>
+				<td>
+					<label>username<label>
+					<form>
+						<input type="text" name="username" id="username"/>
+				</td>
+				<td>
+						<label>password<label>
+						<input type="password" name="password" id="password"/>
+				</td>
+				<td>
+				<!--<input type="submit" value="login" onClick="login()"/>-->
+						<a href="#" onClick="login()">Login</a>
+					</form>
+				</td>
+			</tr>
+		</table>
+		<?php
+	} // end printLoginForm()
+
+	function printLoggedInForm($username)
+	{
+		echo "logged in : $username";
+		echo "<a href='#' onClick='logout()'>Logout</a>";
+	}
 
 
 ?>
