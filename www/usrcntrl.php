94500c1e0befb015887b3448259a626b636f2e6c
Marcel Keller <marcel.keller1993@gmail.com>
2011-05-17 21:43:40 +0200

    first tries of jquery user option pane

--- www/usrcntrl.php
+++ www/usrcntrl.php
@@ -1,65 +1,42 @@
 <?php 
-/* User control page, nach der fertigstellung wird diese Seite auf der mainpage eingebunden und durch einen link über javascript aufgerufen 
+	/* User control page, nach der fertigstellung wird diese Seite auf der mainpage eingebunden und durch einen link über javascript aufgerufen 
 
-Hier können user einstellungen vorgenommen werde, sowie Statistiken werden angezeigt. 
+	Hier können user einstellungen vorgenommen werde, sowie Statistiken werden angezeigt. 
 
-	Statistik
-		- Erfasste Kebabstände
-		- Bewertete Kebabstände
+		Statistik
+			- Erfasste Kebabstände
+			- Bewertete Kebabstände
+		
+		Konfiguration / Anzeige
+			- Username 
+			- Passwort ändern
+			- E-Mail addresse
+			- Konto löschen
+	*/
+
+
+	include 'header.php';
+	include 'classes.php';
+
+
+	$myDB = new Database();
+	$myDB-> connect();
+	?>
 	
-	Konfiguration / Anzeige
-		- Username 
-		- Passwort ändern
-		- E-Mail addresse
-		- Konto löschen
-*/
-
-
-include 'header.php';
-include 'classes.php';
-
-
-$myDB = new Database();
-$myDB-> connect();
-
-
-//Find username in session
-$myUser = new user("marco");
-echo $myUser->username;
-
-
-
-//$sql = mysql_query("select usrName,usrEmail,usrLastLogin, usrCreated from tUser where usrName = 'marco';");
-$sql = mysql_query("select usrName,usrEmail,usrLastLogin, usrCreated from tUser where usrName = '".$myUser->username."'");
-$num_rows = mysql_num_rows($sql);
-
-while ($row = mysql_fetch_array($sql)){
+	<div class="coda-slider preload" id="settings-slider">
+		<div class="panel">
+			<div class="panel-wrapper">
+				<h2 class="title">Change password</h2>
+				<p>blabla</p>
+			</div>
+		</div>
+		<div class="panel">
+			<div class="panel-wrapper">
+				<h2 class="title">Remove account</h2>
+				<p></p>
+			</div>
+		</div>
+	</div><!-- .coda-slider -->
+	
+	<?php
 ?>
-
-  
-<div id="usrcntrl">
-
-   <table>
-    <tr>
-     <td>Username: </td> 
-     <td><?=$row['usrName'];?></td>
-    </tr>
-   <tr>
-     <td>E-Mail:  </td> 
-     <td><?=$row['usrEmail'];?></td>
-    </tr>
-    <tr>
-     <td>Last-Login: </td> 
-     <td><?=$row['usrLastLogin'];?></td>
-    </tr>
-    <tr>
-     <td>Account created: </td> 
-     <td><?=$row['usrCreated'];?></td>
-    </tr>
-  </table>
-
-</div>
-
-<?php
-  }
- ?>
