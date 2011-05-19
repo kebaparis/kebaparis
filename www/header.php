8fa9bcd94f2bb90c5dd4d35a2dd84ce7e3f84510
Klar <arvet.fuchs@gmail.com>
2011-05-19 20:36:32 +0200

    Added toggle css & js style in header

--- www/header.php
+++ www/header.php
@@ -7,6 +7,9 @@
   
     <link rel="stylesheet" type="text/css" href="css/main.css">
 	<link rel="stylesheet" href="css/coda-slider-2.0.css" type="text/css" media="screen" />
+	
+	<!-- Arvet toggle slider registration -->
+	<link rel="stylesheet" href="css/toggle.css" type="text/css"/>
     
     <style type="text/css">
     
@@ -16,7 +19,14 @@
 	
 	<script type="text/javascript" src="js/ajax.js" ></script>
 	
-	<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
+	
+	
+	<!-- All toggles in this file, Arvet -->
+	<script type="text/javascript" src="js/jquery.toggle.js"></script>
+
+
+	
 	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
 	<script type="text/javascript" src="js/jquery.coda-slider-2.0.js"></script>
 	
