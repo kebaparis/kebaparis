6d6eadcd193a6bfce75e93633e90bcc71d421c3f
Klar <arvet.fuchs@hotmail.com>
2011-09-05 18:20:51 +0200

    Added tabs

--- www/index.php
+++ www/index.php
@@ -19,14 +19,29 @@
     <table>
     	<tr> 
     		<td> Search: </td>
-    		<td> <input type="text" name="" value="" />  </td>
-    		<td> <a href=""> Go </a>
+    		<td> <input type="text" name="q" onkeyup="letstype(this.value, '""')" size="20" /> </td>
+    		<td> <a href=""> Go </a> </td>
     	</tr>
 	</table>
+
+
     </div>
 <!-- end div #search --> 
-  
-    <div id="stand">
+
+
+<!-- Arvet test 1 -->
+        <div id="tabvanilla" class="widget">
+
+            <ul class="tabnav">
+                <li><a href="#browse">Browse</a></li>
+                <li><a href="#new">New</a></li>
+                <li><a href="#ranking">Ranking</a></li>
+								<li><a href="#moderator">Moderator</a></li>
+								<li><a href="#kebapowner">Kebapowner</a></li>            
+						</ul>
+
+            <div id="browse" class="tabdiv">
+										<p> browse </p>
 
 		<table>
 
@@ -46,8 +61,29 @@
 			</tr>
 
 		</table>
-    
-	</div> <!-- end #stand -->
+
+
+            </div><!--/browse-->
+            
+            <div id="new" class="tabdiv">
+                <p> new </p>
+            </div><!--/new-->
+            
+            <div id="ranking" class="tabdiv">
+                   <p> ranking </p>
+            </div><!--ranking-->
+
+            <div id="moderator" class="tabdiv">
+                   <p> moderator </p>
+            </div><!--moderator-->
+
+            <div id="kebapowner" class="tabdiv">
+                   <p> kebapowner </p>
+            </div><!--kebapowner-->
+
+        </div><!--/widget-->
+
+<!-- Arvet end test 1 -->
 
 	<div id="promo">
 		<table>
