4cc0dda96f049330e0502c5d9727050dc304000c
root <root@Link.(none)>
2011-09-05 18:11:30 +0200

    Added tables for kebapspot

--- sql/structure.sql
+++ sql/structure.sql
@@ -1,5 +1,7 @@
 #SET FOREIGN_KEY_CHECKS=0;
 
+Drop Database if exists fifthch_kebap;
+Create Database fifthch_kebap;
 USE fifthch_kebap;
 
 DROP TABLE IF EXISTS tUser;
@@ -23,4 +25,62 @@ CREATE TABLE tUser
 ) ENGINE=INNODB;
 
 
+
+#New Shiitt***************************************************************************
+
+#Table for Kebabspot Information
+
+drop table if exists spotinfo;
+create table spot
+(
+	ID_spot integer unsigned not null auto_increment,
+	FK_creator int,
+	#FOREIGN KEY (FK_creator) REFERENCES tUser(usrID),
+	FK_Kebapowner int,
+	#FOREIGN KEY (FK_Kebapowner) REFERENCES tUser(usrID),
+	cdate	timestamp, #create date
+	Picture varchar(255),
+	Price Float,
+	Menu Float,
+	Land varchar(255),
+	Kanton varchar(255),
+	Ort	varchar(255),
+	PLZ varchar(255),
+	X float,
+	Y float,
+	Opentimes varchar(255),
+	Rank int,
+	Primary Key (ID_spot)
+) ENGINE=INNODB;
+
+
+#Alle Bewertungen bewerten von 0 - 5 punkte / 1 punkt ist für uns 0.2 zum rechnen.
+DROP TABLE IF EXISTS rating;
+CREATE TABLE rating
+(
+	ID_rating INT unsigned not null auto_increment,
+	turkness float, 
+	price	float, 
+	taste	float,
+	location float,	#(einrichtung und lage)	 	
+	wait_time float,
+	FK_rater int,		#FK ID rater (user-id)
+	#FOREIGN KEY (FK_rater) REFERENCES tUser(usrID),
+	rating float,
+	FK_spot int,	#fremdschlüssel verlinkung hier für kebapstand
+	#FOREIGN KEY (FK_spot) REFERENCES spotinfo(ID_spotinfo),
+	Primary Key (ID_rating)
+) ENGINE=INNODB;
+
+
 #SET FOREIGN_KEY_CHECKS=1;
+
+	
+#ignore
+#Table for statistic
+#drop table if exists 'ranking'
+#create table 'ranking'
+#'ranking_id' integer unsigned not null auto_increment,
+#'date'	date,	#last ranking update time   #Zeit wird nur einmal gespeichert vom script (timestamp) und dann auf Site angezeigt.
+#'FK_Kebapspot'
+#'Rank'	int,
