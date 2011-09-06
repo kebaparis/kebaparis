ad7635b473a668d01f62a3734153c9fbad5e15ff
Klar <arvet.fuchs@hotmail.com>
2011-09-06 10:57:13 +0200

    Tab site for Browse

---
+++ www/browse.php
@@ -0,0 +1,18 @@
+<table>
+
+			<tr>
+				<td rowspan="3"><div id="map">map</div></td>
+				<td colspan="3" id="ort"><h1>Seeperle<h1></td>
+			</tr>
+
+			<tr>
+				<td id="ort" colspan="3">Stäfa</td>
+			</tr>
+
+			<tr id="buttons">
+				<td> <a href"#"> <a href=""> rate </a> </td>
+				<td> <a href"#"> <a href=""> Edit </a> </td>
+				<td> <a href"#"> <a href=""> New  </a> </td>
+			</tr>
+
+</table>
