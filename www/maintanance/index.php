cb0fd0f8a0a0f0c585f51f92e994fcf44a98515e
koma5 <marco@5th.ch>
2011-09-07 16:14:54 +0200

    made links and user names in latest tweet clickable // removed old maintanance site

--- www/maintanance/index.php
+++ www/maintanance/index.php
@@ -28,7 +28,7 @@
 			}
 			
 			#latesttweet {
-     			font-size:35px;
+     			font-size:25px;
 			
 			
 			}
@@ -41,42 +41,38 @@
 			<p>coming soon <a href="mailto:info@kebaparis.ch">...</a></p>
 			<p id="caption">or later</p>
 			
-			<p id="latesttweet">
-		<?php
-              
-              /*// Your twitter username.
-              $username = "kebaparisch";
-              
-              // Prefix - some text you want displayed before your latest tweet.
-              // (HTML is OK, but be sure to escape quotes with backslashes: for example href=\"link.html\")
-              $prefix = "";
-              
-              // Suffix - some text you want display after your latest tweet. (Same rules as the prefix.)
-              $suffix = "";
-              
-              $feed = "http://search.twitter.com/search.atom?q=from:" . $username . "&rpp=1";
-              
-              function parse_feed($feed) {
-                  $stepOne = explode("<content type=\"html\">", $feed);
-                  $stepTwo = explode("</content>", $stepOne[1]);
-                  $tweet = $stepTwo[0];
-                  $tweet = str_replace("&lt;", "<", $tweet);
-                  $tweet = str_replace("&gt;", ">", $tweet);
-                  return $tweet;
-              }
-              
-              $twitterFeed = file_get_contents($feed);
-              echo stripslashes($prefix) . parse_feed($twitterFeed) . stripslashes($suffix);*/
-              
-              $username='kebaparisch'; // set user name
-              $format='json'; // set format
-              $tweet=json_decode(file_get_contents("http://api.twitter.com/1/statuses/user_timeline/{$username}.{$format}")); // get tweets and decode them into a variable
+			<p id="latesttweet"> latest tweet: 
+			<?php
+				function text2links($str) 
+				{
+						if($str=='' or !preg_match('/(http|www\.|@)/i', $str)) 
+							{
+								return $str; 
+							}
+						$lines = explode("\n", $str); $new_text = '';
 
-              echo $tweet[0]->text; // show latest tweet
-              
-              
-              
-          ?>
+						while (list($k,$l) = each($lines)) { 
+							// replace links:
+							$l = preg_replace("/http:\/\/([^ )\r\n!]+)/i", 
+								"<a href=\"http://\\1\">\\1</a>", $l);
+							$l = preg_replace("/https:\/\/([^ )\r\n!]+)/i", 
+								"<a href=\"https://\\1\">\\1</a>", $l);
+							$l = preg_replace(
+								"/@([a-z0-9A-Z_]{1,15})/", 
+								"<a href=\"http://twitter.com/\\1\">@\\1</a>", $l);
+							$new_text .= $l."\n";
+						}
+						return $new_text;
+				}
+
+
+				$username='kebaparisch'; // set user name
+				$format='json'; // set format
+				$tweet=json_decode(file_get_contents("http://api.twitter.com/1/statuses/user_timeline/{$username}.{$format}")); // get tweets and decode them into a variable
+
+				echo text2links($tweet[0]->text); // show latest tweet
+
+			?>
 		</p>
 		
 	</body>
