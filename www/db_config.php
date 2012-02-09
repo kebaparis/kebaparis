52add6f5d0d68fcc2c62bda9a06c7ec5ffa819cb
koma5 <marco@5th.ch>
2012-02-09 10:26:47 +0100

    readded config files - @Klar don't delete tem again

---
+++ www/db_config.php
@@ -0,0 +1,8 @@
+<?php	
+	// db
+	$this->db_server = '127.0.0.1';
+	$this->db_user = '';
+	$this->db_password = '';
+	$this->db_name = 'kebap';
+
+?>
---
+++ www/mail_config.php
@@ -0,0 +1,12 @@
+<?php
+// Mail config
+
+$from = "kebaparis <do-not-reply@kebaparis.ch>"; //no @signs and dots outside of the <>brackets, message will not be sent
+
+
+$host = "ssl://mail.kebaparis.ch";
+$port = "465";
+$username = "<ourusername>";
+$password = "<ourpassword>";
+
+?>
