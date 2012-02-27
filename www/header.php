4fdcef0023830eea7b508f75e455293c5e9949be
klar <arvet.fuchs@hotmail.com>
2012-02-27 10:38:31 +0100

    ByeBye ajax, css, jquery stuff...

--- www/header.php
+++ www/header.php
@@ -7,12 +7,6 @@
   
     <link rel="stylesheet" type="text/css" href="css/main.css">
 	
-	<!-- Coda Slider CSS -->
-	<link rel="stylesheet" href="css/coda-slider-2.0.css" type="text/css" media="screen" />
-	
-	<!-- Arvet toggle slider registration & login -->
-	<link rel="stylesheet" href="css/toggle.css" type="text/css"/>
-
     
     <style type="text/css">
     
@@ -20,38 +14,8 @@
     
     </style>
 
-
-	<!-- Arvet Tab / Slider thing -->
-	<script type="text/javascript" src="js/jquery-1.2.6.min.js"></script>  
-	<script type="text/javascript" src="js/jquery-ui-personalized-1.5.2.packed.js"></script>  
-	<script type="text/javascript" src="js/tabs.js"></script>  
-
-	<!-- All toggles in this file, Arvet -->
-	<script type="text/javascript" src="js/jquery.toggle.js"></script>
-
 	<!-- Used for Login & Registration -->
 	<script type="text/javascript" src="js/ajax.js" ></script>
-	
-	<!-- ^Klar, Registration Validation with jquery -->
-	<script type="text/javascript" src="js/validation.js" ></script>
-
-	<!-- Keller, Coda Slider -->
-	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-	<script type="text/javascript" src="js/jquery.coda-slider-2.0.js"></script>
-
-	
-	<!-- Keller -->
-	<script type="text/javascript">
-		$().ready(function() {
-			$('#settings-slider').codaSlider();
-		});
-		
-	<!-- Arvet Coda Slider Browse-->
-	$().ready(function() {
-		$('#coda-slider-browse').codaSlider();
-	});
-
-	</script>
 
   
   </head>
